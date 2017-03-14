6f8d3599886c48d7b7e3219d15b248a993f08145
colan connon <cconnon11@gmail.com>
2017-03-13 20:51:27 -0400

    Add binary search tree symbol table

---
+++ com/colanconnon/DataStructures/Graph.java
@@ -0,0 +1,47 @@
+package com.colanconnon.DataStructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by colan on 2/28/2017.
+ */
+public class Graph
+{
+    private final int V;
+    private int E;
+    private List<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (List<Integer>[]) new List[V];
+        for (int v = 0; v < V; v++)
+        {
+            adj[v] = new ArrayList<>();
+        }
+    }
+
+    public int V()
+    {
+        return V;
+    }
+
+    public int E()
+    {
+        return E;
+    }
+
+    public void addEdge(int v, int w)
+    {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v)
+    {
+        return adj[v];
+    }
+
+}
